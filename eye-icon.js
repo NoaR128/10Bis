ffe0399179bbf61535b6aa2ea3e3072c2d06a65f
NoaR128 <noar121@gmail.com>
2024-03-27 16:44:06 +0200

    10Bis

---
+++ eye-icon.js
@@ -0,0 +1,23 @@
+﻿const form= document.querySelector(".form")
+pwShowHide = document.querySelectorAll(".eye-icon");
+link = document.querySelectorAll(".link");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    pwShowHide.forEach(eyeicon => {
+        eyeicon.addEventListener("click", () => {
+            var pwFields = eyeicon.parentElement.parentElement.querySelectorAll(".password");
+
+            console.log(pwShowHide)
+            pwFields.forEach(password => {
+                if (password.type === "password") {
+                    password.type = "text";
+                    eyeicon.classList.replace("bx-hide", "bx-show");
+                    return;
+                }
+                password.type = "password";
+                eyeicon.classList.replace("bx-show", "bx-hide");
+            })
+        })
+    })
+})
